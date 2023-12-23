89a8b186557bdc0c693c2202d583358accacab52
kallempudisaisowjanya <99859571+kallempudisaisowjanya@users.noreply.github.com>
2023-12-23 18:41:12 +0530

    f

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 server.use(router)
 server.listen(4000, () => {
-  console.log('JSON Server is running on 4000')
+  console.log('JSON Server is running on 4000 port')
 })
